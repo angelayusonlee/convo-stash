294026280ab0521111c8d01962741f888f4d6325
Angela Y. Lee <angela8@stanford.edu>
2025-03-03 17:41:36 -0800

    Delete src/utils/getQueryParam.ts

--- src/utils/getQueryParam.ts
+++
@@ -1,5 +0,0 @@
-// src/utils/getQueryParam.ts
-export function getQueryParam(param: string): string | null {
-    const urlParams = new URLSearchParams(window.location.search);
-    return urlParams.get(param);
-}
